0852f073389b83bfaa2396a569b742a4880eefe6
imos <github@imoz.jp>
2014-09-07 22:38:32 +0900

    Fix a bug.

--- src/config/appengine.go
+++ src/config/appengine.go
@@ -5,7 +5,7 @@ import (
 )
 
 func init() {
-	imosrpc.RegisterHandler("config/appengine", DatabaseHandler)
+	imosrpc.RegisterHandler("config/appengine", AppEngineHandler)
 }
 
 type AppEngineRequest struct {}
